c80d4a007825b4065e3464cd5ca1c5e7c00de188
Abdulsamed ALP <47443237+samedalp@users.noreply.github.com>
2026-03-16 03:36:43 +0300

    Error handler changed

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -6,12 +6,9 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\CreateOrderRequest;
 use App\Http\Resources\OrderDetailResource;
 use App\Http\Resources\OrderResource;
-use App\Models\Order;
-use App\Services\Order\CreateOrderService;
 use App\Services\Order\CreateOrderServiceInterface;
 use App\Services\Order\OrderDetailService;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\Cache;
 
 class OrderController extends Controller
 {
